da94c23cffcd9c21dd9fddfa9a8af1ebce335c26
ClementineAccount <ClementineShamaney@gmail.com>
2023-04-25 19:21:48 +0800

    Sphere drawing example

--- src/Project/ProjectApplication.cpp
+++ src/Project/ProjectApplication.cpp
@@ -22,7 +22,7 @@
 #include <vector>
 #include <queue>
 #include <set>
-
+#include <iostream>
 
 
 void ProjectApplication::AfterCreatedUiContext()
@@ -112,10 +112,10 @@ void ProjectApplication::DrawPixelsToScreen()
 void ProjectApplication::DrawPixel(int32_t x, int32_t y, glm::vec4 color, int32_t x_offset, int32_t y_offset)
 {
     //Bounds check. Simply don't draw if out of bounds
-    if (x < 0 || y < 0)
+    if (x + x_offset < 0 || y + y_offset < 0)
         return;
 
-    if (x > windowWidth || y > windowHeight)
+    if (x + x_offset > windowWidth || y + y_offset > windowHeight)
         return;
 
     glTextureSubImage2D(pixel_texture, 0, x + x_offset, y + y_offset, 1, 1, GL_RGBA, GL_FLOAT, glm::value_ptr(color));
@@ -225,7 +225,9 @@ void ProjectApplication::DrawFilledSquare(glm::i32vec2 center, glm::vec4 color, 
 void ProjectApplication::RenderScene([[maybe_unused]] double dt)
 {
     //RenderSceneOne();
-    RenderSceneTwo();
+    //RenderSceneTwo();
+
+    RenderSceneThree(dt);
 }
 
 
@@ -329,9 +331,131 @@ void ProjectApplication::RenderSceneTwo()
     }
     
     DrawPixelsToScreen();
-
-
 }
+
+
+void ProjectApplication::RenderSceneThree([[maybe_unused]] double dt)
+{
+    ClearFBO(screen_draw_fbo, clear_screen_color);
+
+    //Prototyping just drawing in one pass and storing that first
+    //Adapted from Fabriel Gambetta's 'Computer Graphics from Scratch'
+    static bool is_first_draw = true;
+    if (is_first_draw)
+    {
+        Timer timer;
+
+        ClearFBO(pixel_draw_fbo, pixel_clear_screen_color);
+
+        //Origin
+        static glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 0.0f);
+        
+        static int32_t canvas_width = (windowWidth);
+        static int32_t canvas_height = (windowHeight);
+        
+        static float viewport_width = 1.0f;
+        static float viewport_height = 1.0f;
+
+        static float distance_to_viewport = 1.0f;
+
+        static float inf = std::numeric_limits<float>::max();
+
+        static float t_min = distance_to_viewport;
+        static float t_max = inf;
+
+        static glm::vec4 default_draw_color = glm::vec4(1.0f, 1.0f, 1.0f, 1.0f);
+
+        auto convert_canvas_to_viewport = [&](int32_t x, int32_t y)
+        {
+            return glm::vec3(
+                static_cast<float>(x) * (viewport_width / static_cast<float>(canvas_width)), 
+                static_cast<float>(y) * (viewport_height / static_cast<float>(canvas_height)),
+                distance_to_viewport);
+        };
+
+        struct Sphere 
+        {
+            glm::vec3 center;
+            glm::vec3 color;
+            float radius;
+        };
+
+
+
+        auto intersect_ray_sphere = [&](glm::vec3 origin, glm::vec3 ray, glm::vec3 sphere_center, float radius)
+        {
+            glm::vec3 CO = origin - sphere_center;
+
+            //solving with quadratic formula
+            float a = glm::dot(ray, ray);
+            float b = 2 * glm::dot(CO, ray);
+            float c = glm::dot(CO, CO) - radius * radius;
+
+            float discrim = b * b - 4 * a * c;
+
+            //x = t1, y = t2
+            //Negative values are rejected
+            glm::vec2 t_pairs(inf, inf);
+
+            if (discrim < 0)
+            {
+                return t_pairs;
+            }
+
+            t_pairs.x = (-b + sqrt(discrim) / (2 * a));
+            t_pairs.y = (-b - sqrt(discrim) / (2 * a));
+            return t_pairs;
+        };
+
+        Sphere sphere_green{glm::vec3(-2.0f, 0.0f, 4.0f), glm::vec3(0.0f, 1.0f, 0.0f), 1.0f};
+        Sphere sphere_red{glm::vec3(0.0f, -1.0f, 3.0f), glm::vec3(1.0f, 0.0f, 0.0f), 1.0f};
+        Sphere sphere_blue{glm::vec3(2.0f, 0.0f, 4.0f), glm::vec3(0.0f, 0.0f, 1.0f), 1.0f};
+
+        auto TraceRay = [&](glm::vec3 origin, glm::vec3 ray, std::vector<Sphere>const& sphere_list)
+        {
+            float closest_t = inf;
+            glm::vec4 draw_color = default_draw_color; //Background color
+
+            for (auto const& sphere : sphere_list)
+            {
+                glm::vec2 t_pairs = intersect_ray_sphere(origin, ray, sphere.center, sphere.radius);
+
+                auto update_closest = [&](float t_value)
+                {
+                    if (t_value > t_min && t_value < t_max && t_value < closest_t)
+                    {
+                        closest_t = t_value;
+                        draw_color = glm::vec4(sphere.color, 1.0f);
+                    }
+                };
+
+                update_closest(t_pairs.x);
+                update_closest(t_pairs.y);
+            }
+            return draw_color;
+        };
+
+        glm::vec3 origin = glm::vec3(0.0f, 0.0f, 0.0f);
+        std::vector<Sphere> spheres{sphere_red, sphere_green, sphere_blue};
+
+
+        for (int32_t x = -canvas_width / 2; x < canvas_width / 2; x += 1)
+        {
+            for (int32_t y = -canvas_height / 2; y < canvas_height / 2; y += 1)
+            {
+                glm::vec3 ray = convert_canvas_to_viewport(x, y);
+                DrawPixelCentreOrigin(x, y, TraceRay(origin, ray, spheres));
+            }
+        }
+        is_first_draw = false;
+
+        auto ms = timer.Elapsed_us() / 1000;
+        std::cout << "Drawing spheres took " << ms << " ms\n";
+
+    }
+    DrawPixelsToScreen();
+}
+
 
 
 void ProjectApplication::RenderUI([[maybe_unused]] double dt)
--- src/Project/include/Project/ProjectApplication.hpp
+++ src/Project/include/Project/ProjectApplication.hpp
@@ -12,6 +12,35 @@
 #include <memory>
 #include <queue>
 #include <functional>
+#include <chrono>
+
+//Stolen from Fwog
+class Timer
+{
+    using microsecond_t = std::chrono::microseconds;
+    using myclock_t = std::chrono::high_resolution_clock;
+    using timepoint_t = std::chrono::time_point<myclock_t>;
+public:
+    Timer()
+    {
+        timepoint_ = myclock_t::now();
+    }
+
+    void Reset()
+    {
+        timepoint_ = myclock_t::now();
+    }
+
+    double Elapsed_us() const
+    {
+        timepoint_t beg_ = timepoint_;
+        return static_cast<double>(std::chrono::duration_cast<microsecond_t>(myclock_t::now() - beg_).count());
+    }
+
+private:
+    timepoint_t timepoint_;
+};
+
 
 class ProjectApplication final : public Application
 {
@@ -42,15 +71,17 @@ private:
     void DrawPixelLineNaive(int32_t x_start, int32_t y_start, int32_t x_end, int32_t y_end, glm::vec4 color);
     void DrawLineBresenhamNaive(glm::i32vec2 start_pos, glm::i32vec2 end_pos, glm::vec4 color);
     void DrawLineBresenham(glm::i32vec2 start_pos, glm::i32vec2 end_pos, glm::vec4 color, bool centerOrigin = true);
-
-
-
     void DrawFilledSquare(glm::i32vec2 center, glm::vec4 color, int32_t length = 30, bool centerOrigin = false);
 
     //Some 'scenes' which are just collection of function calls
     void BuildSceneOneCommands();
     void RenderSceneOne();
     void RenderSceneTwo();
+
+
+    //Simple sphere projection with rays
+    void RenderSceneThree(double dt);
+
 
     void BrushControlCallback(double xoffset, double yoffset);
 
